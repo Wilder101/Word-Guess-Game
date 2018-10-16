819b866075d0db1fd0ae136b26f197b6c99261c7
GitWilder <43479032+Wilder101@users.noreply.github.com>
2018-10-15 21:08:51 -0700

    more logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,8 @@ var game = {
 
     // Key-value pairs aka property-value pairs aka object's public (?) variables
     wins: 0,
-    currentWord: "",            // update as necessary
+    currentWord: [],            // current word to guess
+    currentWordGuess: [],       // current word that has been guessed
     numGuessesRemain: 12,       // initialize at 12
     lettersGuessed: [],         // initialize empty
     wordDB: [
@@ -35,12 +36,22 @@ var game = {
     // Game user input 
     submitInput: function(userInput) {
 
-        // Add input to letters guessed if not already included in list
+        // Add input to letters guessed if not already included in list & play
         if (!this.lettersGuessed.includes(userInput)) {
             this.lettersGuessed.push(userInput);
+
+            // Display guesses
+            this.displayGuesses();
+
+            // Update number of guesses remaining
+            this.updateNumGuessRemaining();
+
+            // Show user's input in hangman word
+            this.showCurrentWord(userInput);
         }
     },
 
+    // Display guesses helper method
     displayGuesses: function() {
 
         // Local variables
@@ -52,14 +63,34 @@ var game = {
             buildDisplayString.push(" " + this.lettersGuessed[i])
         }
 
-        // Update UI with all letters guessed
+        // Update UI with all letters guessed 
         showLetters.textContent = buildDisplayString;
+    },
+
+    // Update number of guesses remaining helper method
+    updateNumGuessRemaining: function() {
+
+        // Decrement remaining guesses
+        this.numGuessesRemain--;
+
+        // Display guess number remaining in UI
+        var remainingGuesses = document.getElementById("display-num-rem");
+        remainingGuesses.textContent = this.numGuessesRemain;
     },
 
     // Game methods 
     setCurrentWord: function() {
+
         // Get a new random word from word "database"
-        this.currentWord = this.wordDB[Math.floor(Math.random() * this.wordDB.length)];
+        // this.currentWord = this.wordDB[Math.floor(Math.random() * this.wordDB.length)];
+        
+        // Initialize current word
+        this.currentWord = this.wordDB[0];  // TESTING with one word, not random yet
+
+        // Initialize current word guess word
+        for (var i = 0; i < this.currentWord.length; i++) {
+            this.currentWordGuess.push("_");
+        }
     },
 
     showWins: function() {
@@ -67,22 +98,33 @@ var game = {
         showWinsNumber.textContent = this.wins;
     },
 
-    showCurrentWord: function() {
-        // display-word
+    showCurrentWord: function(userInput) {
+
+        // Display word
         var showWord = document.getElementById("display-word");
-        var buildPrintWord;
+        var buildPrintWord = [];
 
         // use a loop for each character in currentWord and compare it to every lettersGuessed
         for (var i = 0; i < this.currentWord.length; i++) {
-            if (this.currentWord[i] === this.lettersGuessed.includes()) {
-                buildPrintWord = buildPrintWord + this.currentWord[i];
-                console.log(buildPrintWord);
-            }
-            else {
-                buildPrintWord = buildPrintWord + " _";
+
+            // if (this.currentWord[i] === this.lettersGuessed.includes()) {
+            //     buildPrintWord = buildPrintWord + this.currentWord[i];
+            //     console.log(buildPrintWord);
+            // }
+            // else {
+            //     buildPrintWord = buildPrintWord + " _";
+            // }
+            if (this.currentWord[i] === userInput) {
+                this.currentWordGuess[i] = userInput;
             }
         }
-        showWord.textContent = buildPrintWord;
+
+        // // TESTING
+        // if (this.currentWord.includes(userInput)) {
+        //     buildPrintWord.push(userInput);
+        // }
+
+        showWord.textContent = this.currentWordGuess;
     }
 
 
